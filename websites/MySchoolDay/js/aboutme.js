847814612aa81567ec15062332e0964f160db3e8
ttsukianto <ttsukianto@hotmail.com>
2016-09-27 08:56:35 -0600

    Class 9/27

--- websites/MySchoolDay/js/aboutme.js
+++ websites/MySchoolDay/js/aboutme.js
@@ -5,4 +5,14 @@ $(document).ready(function() {
 	$("#aboutmebutton").click(function() {
 		$("#aboutmeinfo").fadeToggle(500);
 	});
+	
+	$(".mousechange").mouseenter(function() {
+		$(this).css("font-size", "2.5em");
+		$(this).css("color", "#0000FF");
+	});
+	
+	$(".mousechange").mouseleave(function() {
+		$(this).css("font-size", "2em");
+		$(this).css("color", "#FFFFFF");
+	});
 });
